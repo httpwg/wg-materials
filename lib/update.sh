35463d646892cbfb112f34dffc8b4184c9a51a14
Mark Nottingham <mnot@mnot.net>
2020-10-14 18:09:07 +1100

    try an index workflow

---
+++ lib/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is for CI updates
+
+BRANCH=gh-pages
+
+# setup
+git config user.email mnot@mnot.net
+git config user.name mnot-bot
+git remote set-url --push origin https://mnot:$GITHUB_TOKEN@github.com/httpwg/wg-materials
+git checkout -B $BRANCH origin/$BRANCH
+
+# Push the changes
+git add .
+git commit -m "update indices"
+git push origin BRANCH

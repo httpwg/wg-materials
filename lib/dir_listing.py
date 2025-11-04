a8bf7bbfbdcd4e69cd03f5bea6b62fa2aee9f728
Mark Nottingham <mnot@mnot.net>
2025-11-04 12:11:01 -0500

    not js

--- lib/dir_listing.py
+++ lib/dir_listing.py
@@ -53,7 +53,7 @@ def fetch_summary(meeting):
         summary = [summary_header]
         with open(f"{auto_minutes_path}/{meeting}/.manifest.json") as fh:
             manifest = json.load(fh)
-        sessions = [s.sessionId for s in manifest.sessionGroups 
+        sessions = [s.sessionId for s in manifest['sessionGroups'] 
                     if s["sessionName"] == wgname.upper()]
         for session_id in sessions:
             session_time = "-".join(session_id.rsplit("-", 2)[-2:])
